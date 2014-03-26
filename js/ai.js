2fa4d9da1b84213e344cd7e4d697073466215223
Igal Kreichman <igal@skycure.com>
2014-03-26 23:49:37 +0200

    Try new approach

--- js/ai.js
+++ js/ai.js
@@ -6,19 +6,19 @@ function AI(grid) {
 AI.prototype.eval = function() {
   var emptyCells = this.grid.availableCells().length;
 
-  var smoothWeight = 0.1,
-      //monoWeight   = 0.0,
-      //islandWeight = 0.0,
-      mono2Weight  = 1.0,
-      emptyWeight  = 2.7,
-      maxWeight    = 1.0;
+  var score = 0;
+  var max = 2;
+  for (var i in this.grid.cells) {
+    for (var j in this.grid.cells[i]) {
+      var tile = this.grid.cells[i][j];
+      if (tile != null) {
+        score += (tile.value/2 - 1);
+        max = Math.max(max, tile.value);
+      }
+    }
+  }
 
-  return this.grid.smoothness() * smoothWeight
-       //+ this.grid.monotonicity() * monoWeight
-       //- this.grid.islands() * islandWeight
-       + this.grid.monotonicity2() * mono2Weight
-       + Math.log(emptyCells) * emptyWeight
-       + this.grid.maxValue() * maxWeight;
+  return (max/16.0)*emptyCells.length + score;
 };
 
 // alpha-beta depth first search
@@ -36,7 +36,7 @@ AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
       if (newGrid.move(direction).moved) {
         positions++;
         if (newGrid.isWin()) {
-          return { move: direction, score: 16*newGrid.winScore, positions: positions, cutoffs: cutoffs };
+          return { move: direction, score: 16*Grid.prototype.winScore, positions: positions, cutoffs: cutoffs };
         }
         var newAI = new AI(newGrid);
 
@@ -44,8 +44,8 @@ AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
           result = { move: direction, score: newAI.eval() };
         } else {
           result = newAI.search(depth-1, bestScore, beta, positions, cutoffs);
-          if (result.score > 16*newGrid.winScore) { // win
-            result.score = 16*newGrid.winScore - 1; // to slightly penalize higher depth from win
+          if (result.score > 16*Grid.prototype.winScore) { // win
+            result.score = 16*Grid.prototype.winScore - 1; // to slightly penalize higher depth from win
           }
           positions = result.positions;
           cutoffs = result.cutoffs;
@@ -64,37 +64,16 @@ AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
   } else { // computer's turn, we'll do heavy pruning to keep the branching factor low
     bestScore = beta;
 
-    // try a 2 and 4 in each cell and measure how annoying it is
-    // with metrics from eval
     var candidates = [];
     var cells = this.grid.availableCells();
-    var scores = [];
     for (var i in cells) {
-      scores.push(null);
       var cell = cells[i];
-
-      var tile = new Tile(cell, 2);
-      this.grid.insertTile(tile);
-      scores[i] = (-this.grid.smoothness() + this.grid.islands())*0.9;
-      this.grid.removeTile(cell);
-
-      tile = new Tile(cell, 4);
-      this.grid.insertTile(tile);
-      scores[i] += (-this.grid.smoothness() + this.grid.islands())*0.1;
-      this.grid.removeTile(cell);
-    }
-
-    // now just pick out the most annoying moves
-    var maxScore = Math.max.apply(null, scores);
-    for (var i = 0; i < scores.length; i++) {
-      if (scores[i] == maxScore) {
-        candidates.push( { position: cells[i], value: 2 } );
-        candidates.push( { position: cells[i], value: 4 } );
-      }
+      candidates.push( { position: cell, value: 2 } );
+      candidates.push( { position: cell, value: 4 } );
     }
 
     // search on each candidate
-    for (var i = 0; i < candidates.length; i++) {
+    for (var i in candidates) {
       var position = candidates[i].position;
       var value = candidates[i].value;
       var newGrid = this.grid.clone();
@@ -131,18 +110,14 @@ AI.prototype.iterativeDeep = function() {
   var depth = 0;
   var best;
   do {
-    var newBest = this.search(depth, -10000, 10000, 0 ,0);
+    var newBest = this.search(depth, -16*Grid.prototype.winScore, 16*Grid.prototype.winScore, 0 ,0);
     if (newBest.move == -1) {
-      //console.log('BREAKING EARLY');
       break;
     } else {
       best = newBest;
     }
     depth++;
   } while ( (new Date()).getTime() - start < minSearchTime);
-  //console.log('depth', --depth);
-  //console.log(this.translate(best.move));
-  //console.log(best);
   return best
 }
 
